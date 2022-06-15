d58bdb968db34feb038f111e84aaf0f6490d2291
Sourav <s6hr6h4u3@gmail.com>
2022-06-15 13:42:46 +0530

    added the animated navigation link

--- data/links.js
+++ data/links.js
@@ -12,23 +12,27 @@ const Links = [
         url: "https://t-movie-app.netlify.app",
     },
     {
-        text: "color generator app",
-        url: "https://color-generator-project.pages.dev",
-    },
-    {
         text: "homeVerse - house dealing website",
         url: "https://home-verse.netlify.app",
     },
     {
-        text: "microsoft clone",
-        url: "https://microsoft-clone-1.netlify.app",
+        text: "light box responsive website",
+        url: "https://frontend-project-2-lightbox-website.netlify.app",
     },
     {
         text: "gym-website",
         url: "https://gym-website-1.netlify.app",
     },
     {
-        text: "light box responsive website",
-        url: "https://frontend-project-2-lightbox-website.netlify.app",
+        text: "microsoft clone",
+        url: "https://microsoft-clone-1.netlify.app",
+    },
+    {
+        text: "color generator app",
+        url: "https://color-generator-project.pages.dev",
+    },
+    {
+        text: "animated navigation",
+        url: "https://gsap-navigation.netlify.app",
     },
 ];
